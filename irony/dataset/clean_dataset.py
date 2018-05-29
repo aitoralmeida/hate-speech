6fe97ec0f40920d8ac2aed9a71af876681cc356f
Aitor Almeida <aitoralmeida@gmail.com>
2018-05-29 11:29:40 +0200

    cleaning the dataset

---
+++ irony/dataset/clean_dataset.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 29 10:03:24 2018
+
+@author: aitor
+"""
+
+import csv, json
+
+DATASET_IRONY= "irony-labeled.csv"
+DATASET_IRONY_CLEAN = "irony-labeled-clean.json"
+
+def clean_text(text):
+    text = text.replace('.', '')
+    text = text.replace(',', '')
+    text = text.replace(';', '')
+    text = text.replace(':', '')
+    text = text.replace('?', '')
+    text = text.replace('¿', '')
+    text = text.replace('¡', '')
+    text = text.replace('!', '')
+    text = text.replace('"', '')
+    text = text.replace('#', '')
+    text = text.replace('@', '')
+    text = text.replace('%', '')
+    text = text.replace('&', '')
+    text = text.replace('(', '')
+    text = text.replace(')', '')
+    text = text.replace('[', '')
+    text = text.replace(']', '')
+    text = text.replace('{', '')
+    text = text.replace('}', '')
+    text = text.replace('=', '')
+    text = text.replace('$', '')
+    text = text.replace('€', '')
+    text = text.replace('\n', ' ')
+    text = text.replace('\r', ' ')
+    text = text.replace('*', ' ')
+    text = text.replace('/', ' ')  
+    text = text.strip()
+    return text
+    
+if __name__ == "__main__":
+    print 'starting'
+    rows = []
+    with open(DATASET_IRONY, 'r') as csv_file:
+        reader = csv.reader(csv_file)         
+        for row in reader:
+            text = row[0]
+            text = clean_text(text)
+            label = int(row[1])
+            rows.append([text, label])
+
+    with open(DATASET_IRONY_CLEAN, 'w') as clean:
+        json.dump(rows, clean, indent = 2)
+    
+#    with open(DATASET_IRONY_CLEAN, 'r') as intermediate:
+#        for line in intermediate.readline():
+#            print '-----' + line
+##        with open('irony-labeled-clean.csv', 'w') as target:
+##            print intermediate.read()
+    
+    print 'end'
