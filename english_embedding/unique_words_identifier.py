3404aa35f8897f76498f30d25d49eab94baad643
Aitor Almeida <aitoralmeida@gmail.com>
2018-05-29 11:36:41 +0200

    changed to json files

--- english_embedding/unique_words_identifier.py
+++ english_embedding/unique_words_identifier.py
@@ -5,51 +5,28 @@ Created on Mon May 07 14:39:12 2018
 @author: aitor
 """
 
-import csv, json
+import json
 
-DATASET_IRONY= "./../irony/dataset/irony-labeled.csv"
+DATASET_IRONY= "./../irony/dataset/irony-labeled-clean.json"
 
-datasets_csv = [DATASET_IRONY]
+datasets_json = [DATASET_IRONY]
 
 unique_words = set()
 max_phrase_length = 0
 
-def clean_text(text):
-    text = text.replace('.', '')
-    text = text.replace(',', '')
-    text = text.replace(';', '')
-    text = text.replace(':', '')
-    text = text.replace('?', '')
-    text = text.replace('¿', '')
-    text = text.replace('¡', '')
-    text = text.replace('#', '')
-    text = text.replace('@', '')
-    text = text.replace('%', '')
-    text = text.replace('&', '')
-    text = text.replace('(', '')
-    text = text.replace(')', '')
-    text = text.replace('=', '')
-    text = text.replace('$', '')
-    text = text.replace('€', '')
-    text = text.replace('\n', ' ')
-    text = text.replace('\r', ' ')
-    text = text.replace('*', ' ')
-    text = text.replace('/', ' ')    
-    return text
   
 if __name__ == "__main__":
     print 'Processing datasets'
-    for dataset_csv in datasets_csv:
-        with open(dataset_csv, 'r') as csv_file:
-            print 'Dataset:', dataset_csv 
-            reader = csv.reader(csv_file)
-            for row in reader:
-                text = row[0]
-                text = clean_text(text)
-                words = set(text.split(' '))
-                unique_words = unique_words.union(words)
-                if len(words) > max_phrase_length:
-                    max_phrase_length = len(words)
+    for dataset_json in datasets_json:
+        print 'Dataset:', dataset_json 
+        with open(dataset_json, 'r') as dataset_file:
+            rows = json.load(dataset_file)
+        for row in rows:
+            text = row[0]
+            words = set(text.split(' '))
+            unique_words = unique_words.union(words)
+            if len(words) > max_phrase_length:
+                max_phrase_length = len(words)
                     
     print len(unique_words)
     print max_phrase_length  
