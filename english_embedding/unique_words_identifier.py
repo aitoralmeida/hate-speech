02e2e416890d702f70c69cdfac154f5cc878c998
Aitor Almeida <aitoralmeida@gmail.com>
2018-05-30 15:52:43 +0200

    added changes to create general identifiers for all datasets

--- english_embedding/unique_words_identifier.py
+++ english_embedding/unique_words_identifier.py
@@ -6,10 +6,12 @@ Created on Mon May 07 14:39:12 2018
 """
 
 import json
+from unidecode import unidecode
 
 DATASET_IRONY= "./../irony/dataset/irony-labeled-clean.json"
+DATASET_VALENCE_AROUSAL = "./../valence_arousal/dataset/valence-arousal-clean.json"
 
-datasets_json = [DATASET_IRONY]
+datasets_json = [DATASET_IRONY, DATASET_VALENCE_AROUSAL]
 
 unique_words = set()
 max_phrase_length = 0
@@ -17,12 +19,14 @@ max_phrase_length = 0
   
 if __name__ == "__main__":
     print 'Processing datasets'
+    total_text = ''
     for dataset_json in datasets_json:
         print 'Dataset:', dataset_json 
         with open(dataset_json, 'r') as dataset_file:
             rows = json.load(dataset_file)
         for row in rows:
             text = row[0]
+            total_text = total_text + ' ' + text
             words = set(text.split(' '))
             unique_words = unique_words.union(words)
             if len(words) > max_phrase_length:
@@ -34,6 +38,8 @@ if __name__ == "__main__":
         json.dump(list(unique_words), outfile)
     with open('max_phrase_length.json', 'w') as outfile:
         json.dump(max_phrase_length, outfile)
+    with open('total_text.txt', 'w') as outfile:
+        outfile.write(unidecode(total_text))
     print 'FIN'              
     
                 
